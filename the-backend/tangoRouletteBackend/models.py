516963f98cdd9098de040b40dca09e1012b13032
leodvincci <lpenrose@depaul.edu>
2023-02-24 20:05:15 -0600

    updated database, updated models, updated settings, added admin file, and migrations file

--- the-backend/tangoRouletteBackend/models.py
+++ the-backend/tangoRouletteBackend/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Student(models.Model):
     firstName = models.CharField(max_length=25)
     lastName = models.CharField(max_length=25)
-    isRemote = models.BooleanField
-    num_times_chosen = models.IntegerField
+    isRemote = models.BooleanField(default=False)
+    num_times_chosen = models.IntegerField(default=0)
--- the-backend/tangoRouletteBackend/settings.py
+++ the-backend/tangoRouletteBackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tangoRouletteBackend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
