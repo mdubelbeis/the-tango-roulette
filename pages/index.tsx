406651b90bcdef5ed9e1aa34862847ab6c487c1d
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-25 11:14:57 -0600

    added randomizer logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,19 @@ import Image from "next/image";
 import { GrSearch } from "react-icons/gr";
 import Logo from "public/code-platoon-logo-black.png";
 import { useEffect, useState } from "react";
+import { dummy_data } from "@/dummy_data/dummy_data";
+
+interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  isRemote: boolean;
+  timesGone: number;
+}
 
 const HomePage = () => {
-  const [studentData, setStudentData] = useState<{}>({});
+  const [studentData, setStudentData] = useState<Student[]>();
+  const [theChosenOne, setTheChosenOne] = useState<Student>();
 
   useEffect(() => {
     // This will run only on init render
@@ -13,10 +23,24 @@ const HomePage = () => {
   }, []);
 
   const getStudentData = async () => {
-    const response = await fetch("");
-    const data = await response.json();
+    // try {
+    //   // const response = await fetch("");
+    //   // const data = await response.json();
+    //   // setStudentData(data);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
 
-    setStudentData(data);
+  const randomizer = () => {
+    const RANDOM_NUMBER = Math.floor(Math.random() * dummy_data.length);
+    return RANDOM_NUMBER;
+  };
+
+  const handleButtonClick = () => {
+    const RANDOM_INDEX = randomizer();
+    setTheChosenOne(dummy_data[RANDOM_INDEX]);
+    console.log(dummy_data[RANDOM_INDEX]);
   };
 
   return (
@@ -52,8 +76,15 @@ const HomePage = () => {
       </header>
       <main className="w-screen h-screen bg-primary-teal flex items-center justify-center font-lato">
         <section className="bg-shades-300 w-10/12 py-20 rounded-xl drop-shadow-xl max-w-xl flex flex-col items-center justify-center gap-10 -translate-y-[30%]">
-          <h2 className="text-2xl">Student Name</h2>
-          <button className="bg-primary-orange hover:bg-[#99999] py-3 px-6 rounded-lg text-2xl text-primary-white font-thin tracking-wide hover:text-black drop-shadow-lg hover:drop-shadow-xl active:drop-shadow-sm">
+          <h2 className="text-2xl">
+            {!theChosenOne
+              ? "Who is feeling lucky?"
+              : `${theChosenOne.firstName}`}
+          </h2>
+          <button
+            onClick={handleButtonClick}
+            className="bg-primary-orange hover:bg-[#99999] py-3 px-6 rounded-lg text-2xl text-primary-white font-thin tracking-wide hover:text-black drop-shadow-lg hover:drop-shadow-xl active:drop-shadow-sm"
+          >
             PULL
           </button>
         </section>
