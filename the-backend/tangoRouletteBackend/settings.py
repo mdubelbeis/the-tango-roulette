a3e592c23bd8fcfdfb6fbb0f07801ff18d9d5d0b
leodvincci <lpenrose@depaul.edu>
2023-02-24 19:48:44 -0600

    updated database, updated django settings

--- the-backend/tangoRouletteBackend/settings.py
+++ the-backend/tangoRouletteBackend/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest-framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
