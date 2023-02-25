dd4b46b9aff3b4ba99cedc68010d9649aabe437b
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-25 08:20:29 -0600

    added dummy data for testing

---
+++ dummy_data/dummy_data.ts
@@ -0,0 +1,37 @@
+export const dummy_data = [
+  {
+    id: 1,
+    firstName: "Mason",
+    lastName: "Dubelbeis",
+    isRemote: true,
+    timesGone: 0,
+  },
+  {
+    id: 2,
+    firstName: "Leo",
+    lastName: "Penrose",
+    isRemote: false,
+    timesGone: 0,
+  },
+  {
+    id: 3,
+    firstName: "Dizz",
+    lastName: "Smith",
+    isRemote: true,
+    timesGone: 0,
+  },
+  {
+    id: 4,
+    firstName: "Scuba",
+    lastName: "Steve",
+    isRemote: true,
+    timesGone: 0,
+  },
+  {
+    id: 5,
+    firstName: "lastOne",
+    lastName: "Data",
+    isRemote: true,
+    timesGone: 0,
+  },
+];
