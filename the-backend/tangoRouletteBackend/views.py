58d57303528e10462c2094771c0a297067aed805
Leo D. Penrose <lpenrose@depaul.edu>
2023-02-25 03:31:59 -0600

    created serializers, updated views, updated settings

--- the-backend/tangoRouletteBackend/views.py
+++ the-backend/tangoRouletteBackend/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, JsonResponse, request
 from models import Student
-
+from serializers import StudentSerializer
 
 def get_all_students(request):
-    pass
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return JsonResponse(serializer.data)
+
