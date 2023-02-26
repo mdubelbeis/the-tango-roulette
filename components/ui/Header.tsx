ec99f703f72358c14b1410f37500585ee330540c
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-26 09:26:13 -0600

    fixed input text color

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -19,9 +19,9 @@ const Header: React.FC = () => {
             placeholder="Search..."
             type="text"
             name="search"
-            className="hidden md:block px-3 py-2 rounded-full w-full"
+            className="hidden md:block px-3 py-2 rounded-full w-full text-gray-600"
           />
-          <GrSearch color="" className="hidden md:block absolute right-4 top-3">
+          <GrSearch className="hidden md:block absolute right-4 top-3">
             test
           </GrSearch>
         </label>
