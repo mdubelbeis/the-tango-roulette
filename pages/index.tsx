4945e951543554d87b8ff525797a745f7634ffbe
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-26 20:48:41 -0600

    init menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 const HomePage: React.FC = () => {
   return (
-    <>
-      <h2>TANGO</h2>
-      <div></div>
-    </>
+    <main className="flex flex-col items-center justify-center w-screen h-screen gap-10 font-lato font-medium text-lg">
+      <h2 className="flex flex-col items-center text-4xl tracking-wide font-lato font-semibold">
+        <span>TANGO</span>
+        <span>APPS</span>
+      </h2>
+      <div>
+        <ul className="flex flex-col gap-4 text-center">
+          <li className="py-2 px-4 w-fit mx-auto bg-primary-teal text-primary-white rounded hover:bg-primary-teal-dark hover:cursor-pointer shadow-lg hover:shadow-xl active:shadow-lg">
+            <Link href="the-tango-roulette">The Tango Roulette</Link>
+          </li>
+          <li className="py-2 px-4 w-fit mx-auto bg-primary-teal text-primary-white rounded hover:bg-primary-teal-dark hover:cursor-pointer shadow-lg hover:shadow-xl active:shadow-lg">
+            <Link href="pair-programming-generator">
+              Pair Programming Generator
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 };
 
