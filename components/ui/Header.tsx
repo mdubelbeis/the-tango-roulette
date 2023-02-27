fc95cab1d67d3a5d6ac21c0036efcfee46a1cb4d
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-26 20:48:21 -0600

    added the dates

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -5,7 +5,7 @@ import Logo from "public/code-platoon-logo-black.png";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-between bg-primary-orange text-center p-4 text-shades-800 drop-shadow-xl">
+    <header className="fixed w-screen top-0 left-0 flex items-center justify-between bg-primary-orange text-center p-4 text-shades-800 drop-shadow-xl font-lato font-medium">
       <div className="flex items-center rounded">
         <Link
           href="/"
@@ -17,9 +17,12 @@ const Header: React.FC = () => {
             className="w-32 lg:w-44 xl:w-60"
           />
         </Link>
-        <h1 className="tracking-wider text-2xl md:text-2xl lg:text-3xl xl:text-4xl p-2 text-primary-white">
-          TANGO COHORT
-        </h1>
+        <div>
+          <h1 className="tracking-wider text-2xl md:text-2xl lg:text-3xl xl:text-4xl p-2 text-primary-white">
+            TANGO COHORT
+          </h1>
+          <p className="hidden lg:block">February 06, 2023 - May 23, 2023</p>
+        </div>
       </div>
       <div className="">
         <label className="relative drop-shadow-xl">
