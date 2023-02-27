0c60606aaa52cad9b02248b964464aba470e7a6f
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-27 11:07:18 -0600

    turned off DEBUG mode

--- the-backend/tangoRouletteBackend/settings.py
+++ the-backend/tangoRouletteBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i96i_&92_#b7(rfytw*rgilf*#w$9rubsp#@#n@)n2gp=mdfz8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 CORS_ORIGIN_ALLOW_ALL = True
