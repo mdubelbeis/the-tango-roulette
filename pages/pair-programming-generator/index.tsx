ec7f834f2ed78c0350c3c292f64aad0de658fd79
mdubelbeis <mason.dubelbeis@gmail.com>
2023-03-02 09:39:13 -0600

    init struct

--- pages/pair-programming-generator/index.tsx
+++ pages/pair-programming-generator/index.tsx
@@ -1,15 +1,24 @@
-import { MdConstruction } from "react-icons/md";
+import { dummy_data } from "@/dummy_data/dummy_data";
 
 const PairProgrammingApp: React.FC = () => {
-  return (
-    <div className="font-lato font-thin flex items-center justify-center w-screen h-screen text-2xl md:text-4xl lg:text-6xl">
-      <div className="flex gap-3">
-        <MdConstruction />
-        <h1>COMING SOON</h1>
-        <MdConstruction />
-      </div>
-    </div>
-  );
+  return <main className=""></main>;
 };
+
+export async function getServerSideProps(context) {
+  let data;
+  // Fetch data on server.
+  try {
+    // const response = await fetch("http://127.0.0.1:8000/students/");
+    const response = await fetch("http://100.25.160.162:8000/students/");
+
+    data = await response.json();
+  } catch (error) {
+    data = dummy_data;
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
 
 export default PairProgrammingApp;
